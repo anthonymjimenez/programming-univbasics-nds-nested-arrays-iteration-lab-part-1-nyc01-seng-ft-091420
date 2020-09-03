ec1c87e971b17e716696f25c51d0d91865366aca
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-03 02:17:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,6 @@
 def find_even_values(src)
-  src.length.times { |x|
-      src[x].length.times { |y|
+  src.length.each { |x|
+      src[x].length.each { |y|
         if src[x][y].even?
           puts src[x][y]
         end
